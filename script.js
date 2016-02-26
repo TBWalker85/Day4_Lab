32f4a78d40ec3818e5febdbd98ae61fe585b0316
Thomas Walker <thomasbwalker@gmail.com>
2016-02-26 11:29:22 -0600

    simplified language in last function

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
     button.className = 'myButton';
     button.onclick = clickButton;
     document.body.appendChild(button);
-});
 
 function clickButton() {
     var divNumber = String(document.getElementsByClassName('newSquares').length);
     var newBox = document.createElement('div');
     newBox.id = divNumber;
-    divNumber = parseInt(divNumber, 10);
-    var thisDiv = divNumber;
-    var nextDiv = (divNumber + 1);
-    var prevDiv = (divNumber - 1);
+    //divNumber = parseInt(divNumber, 10);
+    //var thisDiv = divNumber;
+    //var nextDiv = (divNumber + 1);
+    //var prevDiv = (divNumber - 1);
     newBox.className = 'newSquares';
     newBox.addEventListener('click', randomColor);
     newBox.addEventListener('mouseenter', mouseEnter);
     newBox.addEventListener('mouseleave', mouseLeave);
     newBox.addEventListener('dblclick', boxGone);
     document.body.appendChild(newBox);
+}
 
 function randomColor(e) {
     var letters = '0123456789ABCDEF'.split('');
@@ -41,13 +41,25 @@ function mouseLeave(e) {
 
 function boxGone(e) {
     console.log('im working');
-    if (thisDiv%2===0) {
-       document.getElementById(nextDiv).remove();
-     } else if (thisDiv%2>0) {
-       document.getElementById(prevDiv).remove();
-     }
-     else {
-         alert('the element does not exist');
-     }
+    var x = e.target.id;
+    x = parseInt(x, 10);
+    if (x%2===0) {
+        e.target.nextElementSibling.remove();
+    } else if (x%2>0) {
+        e.target.previousElementSibling.remove();
+    } else {
+        alert('the element does not exist');
+                }
 }
-}
+//     if (thisDiv < 1) {
+//        alert('the element does not exist');
+//      } else if (thisDiv%2===0) {
+//        document.getElementById(nextDiv).remove();
+//      }
+//      else {
+//        document.getElementById(prevDiv).remove();
+//      }
+// }
+//}
+
+
